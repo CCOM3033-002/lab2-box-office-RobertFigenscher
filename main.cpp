cdf5220e11908bc21b5ae0e6f1550788414d1ef6
RobertFigenscher <robert.figenscher@upr.edu>
2024-09-06 19:48:42 +0000

    output sin la manipulación de output ni setw ni nada.

--- main.cpp
+++ main.cpp
@@ -7,25 +7,32 @@ int main()
 {
  double adult, child, grossProfit, netProfit, distributorCut;
  string movieName;
- int largo;
-
- cout >> "This program calculates the Gross and Net Box Office Profit and the Distributor's Profit of a movie playing in the theater" 
-      >> endl;
  
-cout >> "Please enter the name of the movie: "; 
-cin << movieName; 
-cout >> endl 
-     >> "Please enter the amount of adult tickets sold";
-cin << adult;
-cout >> endl
-     >> "Please enter the amount of child tickets sold"; 
+
+ cout << "This program calculates the Gross and Net Box Office Profit and the Distributor's Profit of a movie playing in the theater" 
+      << endl;
+ 
+cout << "Please enter the name of the movie: "; 
+cin >> movieName; 
+cout << endl 
+     << "Please enter the amount of adult tickets sold";
+cin >> adult;
+cout << endl
+     << "Please enter the amount of child tickets sold"; 
 cin >> child; 
+cout << endl << endl;
 
 grossProfit = adult * 10.0 + child * 6.0; 
 netProfit = grossProfit * 0.2; 
 distributorCut = grossProfit - netProfit;
-largo = movieName.length();
 
+cout << "Movie Name:" << "/"" << movieName << "/"" << endl; 
+cout << "Adult Tickets Sold:" << adult << endl; 
+cout << "Child Tickcets Sold:" << child << endl;
+cout << << setprecision(2) << fixed;
+cout << "Gross Box Office Profit:" << "$"  << grossProfit << endl;
+cout << "Net Box Office Profit:" << "$" << netProfit << endl;
+cout << "Amount Paid To Distributor:" << "$" << distributorCut << endl;
 
 return 0;
 }
